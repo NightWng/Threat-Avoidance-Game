d19bc6ce40a5d005a74b85bf4cce5ea4df1e5781
NightWng <76580053+NightWng@users.noreply.github.com>
2021-04-02 22:56:47 -0600

    Window Class
    Uses JFrame to format window that game runs in

---
+++ Window.java
@@ -0,0 +1,26 @@
+package game.main; //Gibran's Stuff
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class Window extends Canvas 
+{
+	private static final long serialVersionUID = -4923325076398520880L;
+	
+	public Window(int width, int height, String title, Game game)
+	{
+		JFrame frame = new JFrame(title);
+		
+		frame.setPreferredSize(new Dimension(width,height));
+		frame.setMaximumSize(new Dimension(width,height));
+		frame.setMinimumSize(new Dimension(width,height));
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLocation(10,0);
+		frame.add(game);
+		frame.setVisible(true);
+		game.start();	
+	}
+}
