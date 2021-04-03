0b7fd8dabf2e253ead7fd475d51e0e83fcfd9a58
NightWng <76580053+NightWng@users.noreply.github.com>
2021-04-02 22:45:56 -0600

    ID class
    Small class consisting of an enum to keep track of things

---
+++ ID.java
@@ -0,0 +1,11 @@
+package game.main; //Gibran's Stuff
+
+//special java type used to define collections of constants and methods
+
+public enum ID 
+{
+	Player(),
+	Player2(),
+	BasicEnemy(),
+	Trail();
+}
